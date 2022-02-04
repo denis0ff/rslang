6cf175544a6723ee185cb1fbef2f5f247b0158df
ameshchanka <a.meshchanka.it@gmail.com>
2022-02-04 23:16:26 +0300

    feat: add textbookPage component

--- src/components/textbook/types.tsx
+++ src/components/textbook/types.tsx
@@ -1,6 +1,17 @@
+export type NameSectionType = 'Easy' | 'Normal' | 'Hard' | 'Difficult words'
+
+export type CodeSectionType =
+  | 'A1'
+  | 'A2'
+  | 'B1'
+  | 'B2'
+  | 'C1'
+  | 'C2'
+  | 'difficult'
+
 export interface ISection {
-  name: string
-  code?: string
+  name: NameSectionType
+  code?: CodeSectionType
   first?: number
   last?: number
 }
