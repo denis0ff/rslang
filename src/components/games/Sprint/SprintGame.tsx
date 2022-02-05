44603281fd75a82c4c93ceabe8444afab09eb417
MxKlim <86914815+MxKlim@users.noreply.github.com>
2022-02-06 02:51:55 +0300

    feat: add timer-component

--- src/components/games/Sprint/SprintGame.tsx
+++ src/components/games/Sprint/SprintGame.tsx
@@ -1,6 +1,29 @@
+import React, { useEffect, useState } from 'react'
 import { IGameRunProps } from '../types'
+import Timer from './timer'
+import './styles/style.css'
 
-export const SprintGame = ({ words }: IGameRunProps) => {
-  console.log(words)
-  return <h2>SprintGame (Смотри консоль)</h2>
+const Title = () => <h2>SprintGame</h2>
+
+const Wrapper: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <Title />
+      <Timer />
+    </div>
+  )
+}
+const Container: React.FC = () => {
+  return (
+    <div className="container">
+      <Wrapper />
+    </div>
+  )
+}
+
+// { words }: IGameRunProps
+export const SprintGame = (props: IGameRunProps) => {
+  console.log(props)
+  return <Container />
+ 
 }
