b727db79425d075c6149f9c33592b1e3c0e54f05
ameshchanka <a.meshchanka.it@gmail.com>
2022-02-19 18:13:32 +0300

    feat: add footer to bottom and footer on main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,16 @@
 import styled from 'styled-components'
+import { Container } from '../components/ui/Footer/elements'
+import { Footer } from '../components/ui/Footer/Footer'
 
 const Title = styled.h2``
 
 export const Main = () => {
-  return <Title>Main page</Title>
+  return (
+    <>
+      <Container>
+        <Title>Main page</Title>
+      </Container>
+      <Footer />
+    </>
+  )
 }
