bc021a392dbaf2421e08d054db17168b7883abdd
MxKlim <86914815+MxKlim@users.noreply.github.com>
2022-02-21 04:52:06 +0300

    feat: add game description and in the game from the tutorial

--- src/pages/Sprint.tsx
+++ src/pages/Sprint.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { CustomGame } from '../components/games/CustomGame'
 import { Difficulty } from '../components/games/Difficulty'
+import { NotEnoughWords } from '../components/games/NotEnoughWords'
 import { Result } from '../components/games/Result/Result'
 import { SprintGame } from '../components/games/Sprint/SprintGame'
 import { GameStatus, GameType, IAnswers } from '../components/games/types'
 import { IWord } from '../utils/types'
 
 export const Sprint = () => {
-  const [status, setStatus] = useState(GameStatus.SELECT)
-  const [words, setWords] = useState<IWord[]>([])
+  const { state } = useLocation()
+  const [status, setStatus] = useState(
+    state ? GameStatus.TEXTBOOK : GameStatus.SELECT
+  )
+  const [words, setWords] = useState<IWord[]>((state as IWord[] | null) || [])
   const [answers, setAnswers] = useState<IAnswers>({
     right: [],
     wrong: [],
@@ -42,5 +48,14 @@ export const Sprint = () => {
         setAnswers={setAnswers}
       />
     )
-  return null
+  if (status === GameStatus.TEXTBOOK)
+    return (
+      <CustomGame
+        type={GameType.SPRINT}
+        words={words}
+        setWords={setWords}
+        setStatus={setStatus}
+      />
+    )
+  return <NotEnoughWords setStatus={setStatus} setWords={setWords} />
 }
